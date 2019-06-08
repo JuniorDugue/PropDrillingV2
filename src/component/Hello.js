b474bfc294baeb279cdc3da80ff0363bbe57e411
Junior Dugue <jrdugue@gmail.com>
2019-06-08 07:19:55 -0400

    passed images, added repeat method, practiced more props
    passed images, added repeat method, practiced more props

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -2,9 +2,20 @@ import React from "react";
 
 class Hello extends React.Component {
   render() {
+    let bangs = "!".repeat(this.props.bangs);
+    let questionMark = "?".repeat(this.props.questionMark);
+    let twoDash = "-".repeat(this.props.twoDash);
+
     return (
       <div>
-        <p>Greetings {this.props.to}, from {this.props.from}</p>
+        <img src={this.props.img} alt="lambda"/>
+        <p>
+          Greetings {this.props.to}, from {this.props.from}
+          {bangs}
+        </p>
+
+        <p>node sass {twoDash}save</p>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <Hello 
-      to="Ringo"
-      from="Paul"
+      <Hello
+        to="Ringo"
+        from="Paul"
+        bangs={3}
+        questionMark={5}
+        twoDash={2}
+        img={"https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/4578/s300/untitled.png"}
       />
     </div>
   );
